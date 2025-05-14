7954255e4db89dda06f40d969915e75e19193837
Arppitan <a47.arppitan@gmail.com>
2025-05-15 03:00:18 +0530

    temp mod

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,9 @@
 # app/controllers/api_controller.rb
 class ApiController < ActionController::API
   include ActionController::Cookies
-  
+
   def current_user
+    Rails.logger.info("current_user called. session[:user_id] = #{session[:user_id]}")
     @current_user ||= User.find_by(id: session[:user_id])
   end
 end
